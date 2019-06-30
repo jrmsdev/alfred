be3ec26c63a3147279656771bf9805f88a13951b
Jeremías <jrmsdev@gmail.com>
2019-06-30 20:32:24 -0300

    go generate mechanism

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -eu
+
 PKGLIST="$@"
 BUILDDIR=${GOPATH}/pkg/alfred/build
+
 mkdir -p ${BUILDDIR}
 if test "X" = "X${PKGLIST}"; then
 	PKGLIST='./cmd/...'
 fi
+
+go generate generate.go
+
 for pkg in $(go list ${PKGLIST}); do
 	dst=${pkg/github\.com\/jrmsdev\/alfred\//}
 	ext='.a'
@@ -16,4 +21,5 @@ for pkg in $(go list ${PKGLIST}); do
 	echo "-- build ${dst}"
 	go build -i -o ${dst} ${pkg}
 done
+
 exit 0
---
+++ generate.go
@@ -0,0 +1,6 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package alfred
+
+//go:generate go run ./gen/version
