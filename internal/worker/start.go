0215ee3aa32316fd62b055dc3b394290f000b748
Jeremías <jrmsdev@gmail.com>
2019-07-05 17:48:11 -0300

    pass cmd flags to worker

--- internal/worker/start.go
+++ internal/worker/start.go
@@ -14,13 +14,19 @@ import (
 )
 
 func Start(ctx context.Context, name string) error {
+	var args []string
+	if len(os.Args) > 1 {
+		args = os.Args[1:]
+	} else {
+		args = make([]string, 0)
+	}
 	bin := fpath.Join(alfred.Config.LibDir, "bin", name)
-	log.Debug(bin)
+	log.Debug("%s %v", bin, args)
 	if _, err := os.Stat(bin); err != nil {
 		log.Error(err)
 		return err
 	}
-	cmd := exec.Command(bin)
+	cmd := exec.Command(bin, args...)
 	err := cmd.Run()
 	if err != nil {
 		log.Errorf("web worker: %s", err)
