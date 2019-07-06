c102e590b8b411a2181fbfbc120d7aec95b3e803
Jeremías <jrmsdev@gmail.com>
2019-07-06 02:22:39 -0300

    read initial db status

--- internal/server/core/model/status.go
+++ internal/server/core/model/status.go
@@ -3,11 +3,21 @@
 
 package model
 
+import (
+	"github.com/astaxie/beego/orm"
+)
+
 type Status struct {
 	Id int
 	Status string `orm:"size(50)"`
 }
 
 func GetStatus() string {
+	db := orm.NewOrm()
+	cur := &Status{Id: 0}
+	err := db.Read(cur)
+	if err != nil {
+		return "error"
+	}
 	return "init"
 }
